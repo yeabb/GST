f36448da17c2452b22224c371f90f26617134251
Yeabkal Biru <yeabkalbiru@gmail.com>
2023-09-12 00:08:59 -0400

    Bottom nav menu done

---
+++ app/src/main/java/com/example/gst/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.gst
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.gst.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.miHome -> replaceFragment(Home())
+                R.id.miGas -> replaceFragment(Gas())
+                R.id.miPay -> replaceFragment(Pay())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.flFragment, fragment)
+        fragmentTransaction.commit()
+
+    }
+}
